5337baec254c42e780bf9de9c237fbb8508fbc02
Jacqueline-Kelly <Jacqueline.kelly.2016@gmail.com>
2023-04-03 11:05:37 -0700

    es6 bees functional

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
-class ForagerBee {
+class ForagerBee extends Bee {
   // TODO..
-};
+  constructor(...args) {
+    super(...args);
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  forage(v) {
+    this.treasureChest.push(v);
+  }
+}
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,18 @@
-class RetiredForagerBee {
+class RetiredForagerBee extends ForagerBee {
   // TODO..
-};
+  constructor(...args) {
+    super(...args);
+    this.age = 40;
+    this.job = 'gamble';
+    this.color = 'grey';
+    this.canFly = false;
+  }
+
+  forage() {
+    return 'I am too old, let me play cards instead';
+  }
+
+  gamble(v) {
+    this.treasureChest.push(v);
+  }
+}
